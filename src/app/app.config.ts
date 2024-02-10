d32937b508a72efb2969610c45f1f69f6741b667
Sefa Gür <sefa_guer@hotmail.com>
2024-02-10 21:44:25 +0100

    bug fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"ringoffire-43b70","appId":"1:929897219078:web:6bf14df120121971242f87","storageBucket":"ringoffire-43b70.appspot.com","apiKey":"AIzaSyBZWwNmBgw78EU0yACD10uZdGCsC-uTty4","authDomain":"ringoffire-43b70.firebaseapp.com","messagingSenderId":"929897219078","measurementId":"G-6T50EJV1BT"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase()))]
 };
