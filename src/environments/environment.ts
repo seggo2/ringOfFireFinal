5f8d7efd401ddd418d471cb2bd7f82ffb93bb824
Sefa Gür <sefa_guer@hotmail.com>
2024-02-14 19:31:54 +0100

    Revert "firebase finish"
    This reverts commit 46597e4c0ebee6a90f1f3f213f01ffc40cc8e345.

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase:{
+        "projectId": "ringoffire-43b70",
+        "appId": "1:929897219078:web:6bf14df120121971242f87",
+        "storageBucket": "ringoffire-43b70.appspot.com",
+        "apiKey": "AIzaSyBZWwNmBgw78EU0yACD10uZdGCsC-uTty4",
+        "authDomain": "ringoffire-43b70.firebaseapp.com",
+        "messagingSenderId": "929897219078",
+        "measurementId": "G-6T50EJV1BT"
+    },
+};
